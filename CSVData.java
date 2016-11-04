d04b4bbc4dadde3452a55f8679d18a3a64e97073
JohnAppleseed2001 <rohanwalia2001@gmail.com>
2016-11-04 11:05:26 -0700

    Add files via upload

--- CSVData.java
+++ CSVData.java
@@ -1,287 +1,102 @@
+import java.io.BufferedWriter;
 import java.io.File;
-
+import java.io.FileWriter;
 import java.io.IOException;
-
 import java.util.Scanner;
 
-/***
-
- * @author Rohan W. and Anika 
-
- */
-
 public class CSVData {
+	private static boolean DEBUG = false;
+	private double[][] rawData;
 
-	private double[][] data;
+	private CSVData(double[][] data) {
+		rawData = data;
+	}
+	
+	public static CSVData createDataSet(String filepath, int linesToSkip) {
+		debug("Reading file: " + filepath);
+		
+		String data = readFileAsString(filepath);
+		String[] lines = data.split("\n");
+		
+		debug("Reading " + lines.length + " total lines from file");
+		debug("Using index " + (linesToSkip) + " as header row");
+		
+		String headerLine = lines[linesToSkip];
+		debug("Headers: " + headerLine);
+		
+		String[] headers = headerLine.split(",");
+		debug("Parsed header line into: " + headers.length + " total columns");
+		
+		int startColumn = 0;
+		return createDataSet(filepath, linesToSkip + 1, headers, startColumn);
+	}
+	
+	public static CSVData createDataSet(String filepath, int linesToSkip, String[] columnHeaders, int startColumn) {
+		debug("Reading file: " + filepath);
+		
+		String data = readFileAsString(filepath);
+		String[] lines = data.split("\n");
+		
+		debug("Reading " + lines.length + " total lines from file");
+		
+		int numColumns = columnHeaders.length;
+		debug("Reading " + numColumns + " total columns");
+		
+		int startRow = linesToSkip;
+		
+		// create storage for data
+		double[][] numdata = new double[lines.length - linesToSkip][numColumns];
 
-	private String[] columnNames;
-
-	private int numRows;
-
-	private String filePathToCSV;
-
-	public CSVData(String filepath, String[] columnNames, int startRow) {
-
-		this.filePathToCSV = filepath;
-
-		String dataString = readFileAsString(filepath);
-
-		String[] lines = dataString.split("\n");
-
-		int n = lines.length - startRow;
-
-		this.numRows = n;
-
-		int numCol = columnNames.length;
-
-		this.columnNames = columnNames;
-
-		this.data = new double[n][numCol];
-
-		for (int i = 0; i < lines.length - startRow; i++) {
-
-			String line = lines[startRow + i];
-
+		for (int r = startRow; r < lines.length; r++) {
+			String line = lines[r];
 			String[] coords = line.split(",");
 
-			for (int j = 0; j < numCol; j++) {
-
-				if (coords[j].endsWith("#"))
-
-					coords[j] = coords[j].substring(0, coords[j].length() - 1);
-
+			for (int j = startColumn; j < numColumns; j++) {
+				if (coords[j].endsWith("#")) coords[j] = coords[j].substring(0, coords[j].length()-1);
 				double val = Double.parseDouble(coords[j]);
-
-				data[i][j] = val;
-
+				numdata[r - 1][j - startColumn] = val;
 			}
-
 		}
 
+		return new CSVData(numdata);
 	}
+	
+	public double[][] getAllData() {
+		return rawData;
+	}
+	
+	
+		
+	public static void writeDataToFile(String filePath, String data) {
+		File outFile = new File(filePath);
 
-	private String readFileAsString(String filepath) {
-
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(outFile))) {
+			writer.write(data);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String readFileAsString(String filepath) {
 		StringBuilder output = new StringBuilder();
 
 		try (Scanner scanner = new Scanner(new File(filepath))) {
 
 			while (scanner.hasNext()) {
-
 				String line = scanner.nextLine();
-
 				output.append(line + System.getProperty("line.separator"));
-
 			}
-
 		} catch (IOException e) {
-
 			e.printStackTrace();
-
 		}
 
 		return output.toString();
-	
 	}
 	
-		public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[] columnNames) {
-
-			return new CSVData(filename, columnNames, numLinesToIgnore);
-
+	
+	private static void debug(String string) {
+		if (DEBUG ) {
+			System.err.println(string);
 		}
-
-		/***
-
-		 * Returns a new CSV Data object for a file ignoring lines at the top It
-
-		 * uses the first row as column names. All other data is stored as doubles.
-
-		 * 
-
-		 * @param filename
-
-		 *            the file to read
-
-		 * @param numLinesToIgnore
-
-		 *            number of lines at the top to ignore
-
-		 * @return a CSV data object for that file
-
-		 */
-
-		public static CSVData readCSVFile(String filename, int numLinesToIgnore) {
-
-			return null;
-
-		}
-
-		/***
-
-		 * returns a row
-
-		 * 
-
-		 * @param rowIndex
-
-		 *            index of the row to return
-
-		 * @return the row we're returning
-
-		 */
-
-		public double[] getIndividualRow(int rowIndex) {
-
-			double[] indRow = new double[data.length];
-
-			for(int i = 0; i <data.length; i++){
-
-				indRow[i] = data[rowIndex][i];
-
-			}
-
-			return indRow;
-
-		}
-
-		/***
-
-		 * returns an individual column
-
-		 * 
-
-		 * @param columnIndex
-
-		 *            index of column being returned
-
-		 * @return the column we're returning
-
-		 */
-
-		public double[] getIndividualColumn(int columnIndex) {
-
-			double[] indCol = new double[data.length];
-
-			for(int i = 0; i <data.length; i++){
-
-				indCol[i] = data[i][columnIndex];
-
-			}
-
-			return indCol;
-
-		}
-
-		/***
-
-		 * returns multiple rows
-
-		 * 
-
-		 * @param startIndex
-
-		 *            first index of rows
-
-		 * @param endIndex
-
-		 *            last index of rows being returned
-
-		 * @return the rows that we're returning
-
-		 */
-		
-		public double[][] getRows(int startIndex, int endIndex) {
-
-			int index = 0;
-
-			double[][] multipleRows =  new double [data.length][endIndex-startIndex];
-
-			for(int g = startIndex; g < endIndex; g++){
-
-				for(int i = 0; i < data.length-1; i++){
-
-					multipleRows[i][index] = data[i][g];
-
-					index++;
-
-				}
-
-			}
-
-			return multipleRows;
-
-		}
-
-		/***
-
-		 * returns multiple columns
-
-		 * 
-
-		 * @param startIndex
-
-		 *            first index of columns
-
-		 * @param endIndex
-
-		 *            last index of columns
-
-		 * @return the columns that we're returning
-
-		 */
-
-		public double[][] getColumns(int startIndex, int endIndex) {
-
-			int count  = 0;
-
-			double[][] multipleColumns =  new double [data.length][endIndex-startIndex];
-
-			for(int g = startIndex; g < endIndex; g++){
-
-				for(int i = 0; i < data.length; i++){
-
-					multipleColumns[count][i] = data[g][i];
-
-				}
-
-				count ++;
-
-			}
-
-			return multipleColumns;
-
-		}
-
-		/***
-
-		 * 
-
-		 * @param rowIndex
-
-		 *            index of the row to return
-
-		 * @param columnIndex
-
-		 *            index of the column to return
-
-		 * @param newValue
-
-		 *            new value to set existing entry to @ return new Individual
-
-		 * entry
-
-		 */
-
-		public void setIndividualEntry(int rowIndex, int columnIndex, double newValue) {
-
-			data[rowIndex][columnIndex] = newValue;
-
-		}
-
-		public void saveToFile(String filename) {
-			//Not important right now 
-		}
-
 	}
-
-
+}
--- NaiveData.java
+++ NaiveData.java
@@ -1,45 +1,95 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Arrays;
 
 public class NaiveData {
-	
+
+	//"C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/walkingSampleData-out.csv"
+	//"C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/64StepsInPocketJogging-out.csv"
+	//"C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/64StepsInHandJogging-out.csv"
+
 	public static void main (String[] args){
-		CSVData a = CSVData ;
-		double[][] sensorData = ;
+		
+		
 		
 	}
-	
-	
-	private static int countSteps(double[][] sensorData){
+
+	public static void ChangeTime(double[][] sensorData) {
+		int changeAmount = (int) sensorData[0][0];
+		for(int i = 0; i < sensorData.length-1; i++){
+
+			sensorData[i][0] = sensorData[i][0] - changeAmount;
+
+		}
+
+	}
+
+	public static Writer writeDatatoFile (){
+
+		Writer writer = null;
+
+		try {
+
+			writer = new BufferedWriter(new OutputStreamWriter(
+
+					new FileOutputStream("filename.txt"), "utf-8"));
+
+			writer.write("Something");
+
+		} catch (IOException ex) {
+
+		} finally {
+
+			try {writer.close();} catch (Exception ex) {/*ignore*/}
+
+		}
+
+		return writer;
+
+	}
+
+	public static int countSteps(double[][] sensorData, double time){
+
 		double[] Mag = calculateMagFor(sensorData);
 		double mean = calculatemean(Mag);
 		double standarDev = calculateStandardDeviation(Mag, mean);
-		double allow = mean + standarDev;
+		double allow = mean + 0.45 * standarDev;
 		int stepCount = 0;
+		int g = (int) (300/time);
 		
-		for(int i = 0; i < Mag.length-1; i++){
-			if(Mag[i] > Mag[i-1] && Mag[i] > Mag[i+1] && Mag[i] > allow)
-				
+		for(int i = 1; i < Mag.length - 1; i++){
+
+			if(Mag[i] > Mag[i-1] && Mag[i] > Mag[i+1] && Mag[i] >= allow) {
+
 				stepCount ++;
-			
+
+				i += g; // every interval of mag is 0.111 of a second so a step cannot be taken in less than 0.3 seconds
+
+			} 
+
 		}
-		
+
 		return stepCount; 
-	
+
 	}
 
 	public static double calculateMag(double x, double y, double z){
-		return Math.sqrt(x*x + y*y + z*z );
+		return Math.sqrt(x*x + y*y + z*z);
 	}
 
 	private static double[] calculateMagFor(double[][] sensorData){
-		double[] mag = new double[sensorData.length];
+		double[] mag = new double[sensorData.length-1];
 
 		int tempX = 0;
 		int tempY = 0;
 		int tempZ = 0;
 
-		for(int i = 0; i < sensorData.length; i ++){
+		for(int i = 0; i < sensorData.length-1; i ++){
 
-			for(int col = 0; col < sensorData[0].length; col++){
+			for(int col = 1; col < sensorData[0].length-1; col++){
 
 				if(col == 0) tempX = (int) sensorData[i][col];
 				if(col == 1) tempY = (int) sensorData[i][col];
@@ -55,24 +105,24 @@ public class NaiveData {
 	}
 
 	private static double calculateStandardDeviation(double[] mag, double mean){
-		
+
 		double temp = 0;
-		
+
 		for(int i = 0; i < mag.length-1; i ++){
-		
+
 			temp += (mag[i] - mean) * (mag[i] - mean);
-		
+
 		}
-		
+
 		return 	Math.sqrt(temp / mag.length-1);
-	
+
 	}
 
 	private static double calculatemean(double[] mag){
 
 		double total = 0;
 
-		for(int i = 0; i < mag.length; i++) {
+		for(int i = 0; i < mag.length - 1; i++) {
 			total += mag[i]; 
 		}
 
