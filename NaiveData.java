c5c4e184d516e462d52d9849efcddf5491ae5c60
JohnAppleseed2001 <rohanwalia2001@gmail.com>
2016-11-11 23:50:52 -0800

    Add files via upload

--- NaiveData.java
+++ NaiveData.java
@@ -3,20 +3,39 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.util.Arrays;
 
 public class NaiveData {
 
-	//"C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/walkingSampleData-out.csv"
-	//"C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/64StepsInPocketJogging-out.csv"
-	//"C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/64StepsInHandJogging-out.csv"
+
 
 	public static void main (String[] args){
 		
 		
+		CSVData a = CSVData.createDataSet("C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/Slow_walk.txt.txt", 0);
+		double[][] data = a.getAllData();
+		System.out.println(countStepsX(data));
+		
+		CSVData b = CSVData.createDataSet("C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/Med_walk.txt.txt", 0);
+		double[][] data1 = b.getAllData();
+		System.out.println(countStepsX(data1));
+		
+		CSVData c = CSVData.createDataSet("C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/Fast_walk.txt.txt", 0);
+		double[][] data2 = c.getAllData();
+		System.out.println(countStepsX(data2));
+		
+		CSVData d = CSVData.createDataSet("C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/Jog.txt.txt", 0);
+		double[][] data3 = d.getAllData();
+		System.out.println(countStepsX(data3));
+		
+		CSVData e = CSVData.createDataSet("C:/Users/Rohan/workspace/StepCounter/NoiseSmoothing-master/data/Sprint.txt.txt", 0);
+		double[][] data4 = e.getAllData();
+		System.out.println(countStepsX(data4));
 		
 	}
-
+	/****
+	 * This method allows the user to turn unix into time since data has been collecting
+	 * @param sensorData the double array that is retrieved from the original CSV file
+	 */
 	public static void ChangeTime(double[][] sensorData) {
 		int changeAmount = (int) sensorData[0][0];
 		for(int i = 0; i < sensorData.length-1; i++){
@@ -27,46 +46,51 @@ public class NaiveData {
 
 	}
 
-	public static Writer writeDatatoFile (){
-
-		Writer writer = null;
-
-		try {
-
-			writer = new BufferedWriter(new OutputStreamWriter(
-
-					new FileOutputStream("filename.txt"), "utf-8"));
-
-			writer.write("Something");
-
-		} catch (IOException ex) {
-
-		} finally {
-
-			try {writer.close();} catch (Exception ex) {/*ignore*/}
-
-		}
-
-		return writer;
-
-	}
-
-	public static int countSteps(double[][] sensorData, double time){
-
-		double[] Mag = calculateMagFor(sensorData);
+	/****
+	 * This method takes the double array of all the data and counts the number of steps in the 
+	 * @param sensorData the double array that is retrieved from the original CSV file
+	 * @return returns the number of steps in the data
+	 */
+	
+	public static int countSteps(double[][] sensorData){
+		double time = sensorData[0][0]- sensorData[0][1];
+		double[] Mag = NaiveData.calculateMagFor(sensorData);
 		double mean = calculatemean(Mag);
 		double standarDev = calculateStandardDeviation(Mag, mean);
 		double allow = mean + 0.45 * standarDev;
 		int stepCount = 0;
-		int g = (int) (300/time);
-		
+		int instanceChanger = (int) (300/time);
+
 		for(int i = 1; i < Mag.length - 1; i++){
 
 			if(Mag[i] > Mag[i-1] && Mag[i] > Mag[i+1] && Mag[i] >= allow) {
 
 				stepCount ++;
 
-				i += g; // every interval of mag is 0.111 of a second so a step cannot be taken in less than 0.3 seconds
+				i += instanceChanger;
+
+			} 
+
+		}
+
+		return stepCount; 
+
+	}
+	
+	public static int countStepsX(double[][] sensorData){
+		
+		double[] Mag = NaiveData.calculateMagFor(sensorData);
+	
+		int stepCount = 0;
+		
+
+		for(int i = 1; i < Mag.length - 1; i++){
+
+			if(Mag[i] > Mag[i-1] && Mag[i] > Mag[i+1]) {
+
+				stepCount ++;
+
+				
 
 			} 
 
@@ -76,11 +100,23 @@ public class NaiveData {
 
 	}
 
+	/***
+	 * 3d distance formula
+	 * @param x acc x
+	 * @param y acc y
+	 * @param z acc z
+	 * @return distance formula using those three inputs
+	 */
 	public static double calculateMag(double x, double y, double z){
 		return Math.sqrt(x*x + y*y + z*z);
 	}
-
-	private static double[] calculateMagFor(double[][] sensorData){
+	/***
+	 * This method takes the original data and creates a double array
+	 * which is filled with the combination of the x, y ,z vectors from the original data
+	 * @param sensorData the double array that is retrieved from the original CSV file
+	 * @return a double array that is a filled with numbers which are a combination of the x, y, z accelerometer vectors
+	 */
+	public static double[] calculateMagFor(double[][] sensorData){
 		double[] mag = new double[sensorData.length-1];
 
 		int tempX = 0;
@@ -103,7 +139,12 @@ public class NaiveData {
 
 		return mag;
 	}
-
+	/***
+	 * 
+	 * @mag a double array that is a filled with numbers which are a combination of the x, y, z accelerometer vectors
+	 * @param mean
+	 * @return
+	 */
 	private static double calculateStandardDeviation(double[] mag, double mean){
 
 		double temp = 0;
@@ -117,7 +158,11 @@ public class NaiveData {
 		return 	Math.sqrt(temp / mag.length-1);
 
 	}
-
+	/***
+	 * this calculates the mean over an input double array
+	 * @param mag a double array that is a filled with numbers which are a combination of the x, y, z accelerometer vectors
+	 * @return the mean of the double array
+	 */
 	private static double calculatemean(double[] mag){
 
 		double total = 0;
@@ -127,6 +172,24 @@ public class NaiveData {
 		}
 
 		return total / mag.length;
+
+	}
+	/***
+	 * This smooths out all the small bumps in the data
+	 * @param mean a double array that is a filled with numbers which are a combination of the x, y, z accelerometer vectors
+	 * @return new double array that has been smoothed out
+	 */
+	public static double[] Smooth(double[] mean){
+
+		double[] ret = new double[mean.length];
+
+		for(int i = 1; i < mean.length-1; i++){
+
+			ret[i-1] = (mean[i-1] + mean[i])/2; 
+
+		}
+
+		return ret;
 
 	}
 
